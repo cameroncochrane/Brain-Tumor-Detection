4ce398b5699987fc78efd3b89ac27d329814f479
Cameron Cochrane <cochranecameron1@gmail.com>
2026-03-22 05:59:47 +0000

    Started development of the Streamlit-based front-end portion of the project along with further dissemination of model evaluation results.

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+######### BRAIN TUMOR DETECTION APP ############################
+
+# Front end modules:
 import streamlit as st
+
+# Back end modules:
+import sys
+import os
+
+# Custom modules:
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
